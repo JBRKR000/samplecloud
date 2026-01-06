9db4f0a52a5fcef1777eee05c2ff110d758b8970
JBRKR000 <kozimorjakub@gmail.com>
2026-01-06 15:08:12 +0100

    feat: integrate BetterAuth with PostgreSQL adapter and update Prisma configuration
    - Added BetterAuth for authentication with email and password support.
    - Configured PostgreSQL adapter for Prisma.
    - Created necessary API routes for authentication.
    - Defined Prisma schema and migration for user, session, account, and verification models.
    - Updated package dependencies for BetterAuth and Prisma.
    - Added connection pooling for PostgreSQL in Prisma client setup.

---
+++ app/(lib)/prisma/prisma.ts
@@ -0,0 +1,20 @@
+import { PrismaClient } from "@/app/generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { Pool } from "pg";
+
+const globalPrisma = globalThis as unknown as {prisma?: PrismaClient}
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+const adapter = new PrismaPg(pool);
+
+export const prisma = globalPrisma.prisma ?? new PrismaClient({
+    adapter,
+    log: process.env.NODE_ENV === "development" ? ["error", "warn"] : ["error"],
+})
+
+if (process.env.NODE_ENV === "development"){
+    globalPrisma.prisma = prisma;
+}
---
+++ auth.ts
@@ -0,0 +1,15 @@
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { nextCookies } from "better-auth/next-js";
+import { prisma } from "./app/(lib)/prisma/prisma";
+
+export const auth = betterAuth({
+    database: prismaAdapter(prisma, {
+        provider: "postgresql",
+    }),
+    emailAndPassword: {
+        enabled: true
+    },
+    trustedOrigins: [process.env.APP_URL??"http://localhost:3000"],
+    plugins: [nextCookies()]
+});
